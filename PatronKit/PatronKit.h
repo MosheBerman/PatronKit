a0cf9307813068591239ea695fbd3903f53fbb66
Moshe Berman <moshberm@gmail.com>
2016-01-18 23:50:37 -0500

    Initial Commit

---
+++ PatronKit/PatronKit.h
@@ -0,0 +1,19 @@
+//
+//  PatronKit.h
+//  PatronKit
+//
+//  Created by Moshe Berman on 1/18/16.
+//  Copyright © 2016 Moshe Berman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PatronKit.
+FOUNDATION_EXPORT double PatronKitVersionNumber;
+
+//! Project version string for PatronKit.
+FOUNDATION_EXPORT const unsigned char PatronKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PatronKit/PublicHeader.h>
+
+
