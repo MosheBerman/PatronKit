f58807b42253617cf69b9b2cf228f4f016da32f8
Moshe Berman <moshberm@gmail.com>
2016-01-20 17:32:08 -0500

    Added some block types.

---
+++ PatronKit/PatronKitBlockTypes.swift
@@ -0,0 +1,43 @@
+//
+//  PatronKitBlockTypes.swift
+//  PatronKit
+//
+//  Created by Moshe Berman on 1/19/16.
+//  Copyright © 2016 Moshe Berman. All rights reserved.
+//
+
+import StoreKit
+import CloudKit
+
+/**
+ 
+ A block called after fetching `SKProduct`s from the server.
+ 
+ - parameter products : An array of `SKProduct` objects if the fetch completed successfully, otherwise `nil`.
+ - parameter error : Populated withy information if the fetch failed, otherwise `nil`.
+ 
+*/
+typealias FetchProductsCompletionHandler = (products : [SKProduct]?, error : NSError?) -> Void
+
+/**
+
+ A closure called after a StoreKit purchase completes.
+ 
+ - parameter success : `true` if the purchase successfully completed, otherwise `false`.
+ - parameter error : Populated with information if the purchase failed, otherwise `nil`.
+ 
+*/
+
+typealias PurchaseCompletionHandler = (success : Bool, error : NSError?) -> Void
+
+/**
+ 
+ A closure called after a StoreKit restore completes.
+ 
+ - parameter success : `true` if the restore successfully completed, otherwise `false`.
+ - parameter error : Populated with information if the restore failed, otherwise `nil`.
+ 
+ */
+
+typealias RestorePurchasesCompletionHandler = (success : Bool, error : NSError?) -> Void
+
