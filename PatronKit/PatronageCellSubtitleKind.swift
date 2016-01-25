4e6524aa67f168a41172b286308876fa434f8b49
Moshe Berman <moshberm@gmail.com>
2016-01-24 19:14:57 -0500

    Added a subtitle cell kind.

---
+++ PatronKit/PatronageCellSubtitleKind.swift
@@ -0,0 +1,20 @@
+//
+//  PatronageCellSubtitleKind.swift
+//  PatronKit
+//
+//  Created by Moshe Berman on 1/24/16.
+//  Copyright © 2016 Moshe Berman. All rights reserved.
+//
+
+import UIKit
+
+class PatronageCellSubtitleKind: UITableViewCell {
+
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: .Subtitle, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
