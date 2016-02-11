6611e139dbaa4f54747176e91bb2d3e33b4d7902
Moshe Berman <moshefromsinai@gmail.com>
2016-02-11 07:48:23 -0500

    Began work on new UI, by adding a storyboard with some custom UI.

---
+++ PatronKit/Custom Controls/RoundedLabel.swift
@@ -0,0 +1,29 @@
+//
+//  RoundedLabel.swift
+//  PatronKit
+//
+//  Created by Moshe Berman on 2/11/16.
+//  Copyright © 2016 Moshe Berman. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class RoundedLabel: UILabel {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    override func willMoveToSuperview(newSuperview: UIView?) {
+        self.backgroundColor = self.tintColor.colorWithAlphaComponent(0.2)
+        self.layer.cornerRadius = 5.0
+        self.layer.masksToBounds = true
+        self.textAlignment = .Center
+        self.textColor = self.tintColor
+    }
+
+}
