ddfd36f1aba34873a4893288bbcb72555ac8bf84
Moshe Berman <moshefromsinai@gmail.com>
2016-02-11 21:51:23 -0500

    Added custom cell classes.

---
+++ PatronKit/Cells/PatronageOptionTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  PatronageOptionTableViewCell.swift
+//  PatronKit
+//
+//  Created by Moshe Berman on 2/11/16.
+//  Copyright © 2016 Moshe Berman. All rights reserved.
+//
+
+import UIKit
+
+class PatronageOptionTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var productLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ PatronKit/Cells/PlainTextTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  PlainTextTableViewCell.swift
+//  PatronKit
+//
+//  Created by Moshe Berman on 2/11/16.
+//  Copyright © 2016 Moshe Berman. All rights reserved.
+//
+
+import UIKit
+
+class PlainTextTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var primaryLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
